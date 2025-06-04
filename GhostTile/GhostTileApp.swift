13e4a50dce4f2eea6f37dd025ec851c21be17e6c
cikoedofebrians <ciko.edo.febrians@gmail.com>
2025-06-04 15:43:55 +0700

    Initial Commit

---
+++ GhostTile/GhostTileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostTileApp.swift
+//  GhostTile
+//
+//  Created by Ciko Edo Febrian on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GhostTileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
